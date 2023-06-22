8cd9b6c1e4cfb38f0464caf4ab9002298ea06b1c
Nicholas <nicholaswalker961@gmail.com>
2023-06-21 21:06:20 -0400

    Made some changes.

--- src/blackjackapplication/BlackjackPlayer.java
+++ src/blackjackapplication/BlackjackPlayer.java
@@ -8,6 +8,36 @@ package blackjackapplication;
  *
  * @author jassk
  */
-public class BlackjackPlayer {
-    
+public class BlackjackPlayer extends Player {
+    public BlackjackPlayer(String name) {
+        super(name);
+    }
+
+    public int getHandValue() {
+        int value = 0;
+        int numAces = 0;
+
+        for (Card card : getHand()) {
+            value += card.getValue();
+            if (card.getRank().equals("Ace")) {
+                numAces++;
+            }
+        }
+
+        while (value > 21 && numAces > 0) {
+            value -= 10;
+            numAces--;
+        }
+
+        return value;
+    }
+
+    public boolean isBust() {
+        return getHandValue() > 21;
+    }
+
+    @Override
+    public void play() {
+        // Logic to be implemented for the player's turn in Blackjack
+    }
 }
