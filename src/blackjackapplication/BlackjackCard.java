379c4db020fb8e2b53bac2af972caaae77cdee0f
Jass16 <133935720+Jass16@users.noreply.github.com>
2023-06-21 20:48:52 -0400

    Update BlackjackCard.java
    comments added

--- src/blackjackapplication/BlackjackCard.java
+++ src/blackjackapplication/BlackjackCard.java
@@ -1,3 +1,5 @@
+package blackjackapplication;
+
 public class BlackjackCard extends Card {
     public BlackjackCard(String suit, String rank) {
         super(suit, rank);
@@ -7,10 +9,13 @@ public class BlackjackCard extends Card {
     public int getValue() {
         String rank = getRank();
         if (rank.equals("Ace")) {
+            // Ace has a value of 11 in Blackjack
             return 11;
         } else if (rank.equals("King") || rank.equals("Queen") || rank.equals("Jack")) {
+            // King, Queen, and Jack have a value of 10 in Blackjack
             return 10;
         } else {
+            // For numbered cards (2-10), the value is equal to the rank
             return Integer.parseInt(rank);
         }
     }
